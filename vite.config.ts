c4fcd5d26e80316d13f4a2fc365890a15ef1c562
zcj <18137693952@163.com>
2023-10-29 19:18:10 +0800

    feat: 部署配置更新

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }]
-  }
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'serve' ? '' : '/leafletjsExample/nearbyPowerBank/',
+    plugins: [vue()],
+    resolve: {
+      alias: [{ find: '@', replacement: resolve(__dirname, 'src') }]
+    }
+  };
 });
