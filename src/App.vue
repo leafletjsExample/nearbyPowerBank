4a159bd973a84976edf6c404e9affc08d7976b55
zcj <18137693952@163.com>
2023-10-29 15:22:13 +0800

    feat: 初始化地图

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { defineAsyncComponent } from 'vue';
+
+const InitMap = defineAsyncComponent(() => import('@/components/InitMap.vue'));
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="map-box">
+    <init-map class="map"></init-map>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.map-box {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+
+  .map {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 }
 </style>
---
+++ src/components/InitMap.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+import L from 'leaflet';
+
+defineProps({
+  width: {
+    type: String,
+    default: '100%'
+  },
+  height: {
+    type: String,
+    default: '100%'
+  }
+});
+
+const emit = defineEmits(['mapLoad']);
+
+const mapRef = ref();
+
+const initMap = () => {
+  const map = L.map(mapRef.value, {
+    center: [32.0237855, 118.8075675],
+    zoom: 11,
+    minZoom: 6,
+    maxZoom: 20
+  });
+
+  const mapType = 'vec';
+  L.tileLayer(
+    'https://t{s}.tianditu.gov.cn/' +
+      mapType +
+      '_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=' +
+      mapType +
+      '&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}&tk=b72aa81ac2b3cae941d1eb213499e15e',
+    {
+      subdomains: ['0', '1', '2', '3', '4', '5', '6', '7'],
+      attribution:
+        '&copy; <a href="http://lbs.tianditu.gov.cn/home.html">天地图 GS(2022)3124号 - 甲测资字1100471</a>'
+    }
+  ).addTo(map);
+
+  const mapLabelType = 'cva';
+  L.tileLayer(
+    'https://t{s}.tianditu.gov.cn/' +
+      mapLabelType +
+      '_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=' +
+      mapLabelType +
+      '&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}&tk=b72aa81ac2b3cae941d1eb213499e15e',
+    {
+      subdomains: ['0', '1', '2', '3', '4', '5', '6', '7']
+    }
+  ).addTo(map);
+
+  // 地图初始化完成发送事件
+  emit('mapLoad', map);
+
+  return map;
+};
+
+const mapObj = ref();
+
+// 在 onMounted 中初始化地图
+onMounted(() => {
+  mapObj.value = initMap();
+});
+
+const removeMap = () => {
+  if (mapObj.value) {
+    mapObj.value.remove();
+  }
+};
+
+// 在组件卸载时删除地图
+onUnmounted(() => {
+  removeMap();
+});
+</script>
+
+<template>
+  <div ref="mapRef" class="map"></div>
+</template>
+
+<style scoped>
+.map {
+  width: v-bind(width);
+  height: v-bind(height);
+  z-index: 0;
+}
+</style>
