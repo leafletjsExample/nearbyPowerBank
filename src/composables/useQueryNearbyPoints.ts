c9d69ad7be181c4319a950e75722100cf9814348
zcj <18137693952@163.com>
2023-10-30 20:56:57 +0800

    feat: 增加 useQueryNearbyPoints 代码注释

--- src/composables/useQueryNearbyPoints.ts
+++ src/composables/useQueryNearbyPoints.ts
@@ -8,6 +8,7 @@ import L from 'leaflet';
 export const useQueryNearbyPoints = (mapObj: Ref) => {
   const circleOverlay = ref();
 
+  // 清除绘制的圆形覆盖物
   const clearCircleOverlay = () => {
     if (circleOverlay.value) {
       circleOverlay.value.remove();
@@ -16,7 +17,7 @@ export const useQueryNearbyPoints = (mapObj: Ref) => {
   };
 
   /**
-   * 获取一个圆的 geoJson
+   * 获取一个圆的 geoJson 并加载这个圆到地图上
    * @param center 圆的中心点
    * @param radius 圆的半径
    * @param units 半径单位  miles, kilometers, degrees, or radians
@@ -26,6 +27,7 @@ export const useQueryNearbyPoints = (mapObj: Ref) => {
     radius: number,
     units = 'kilometers'
   ) => {
+    // 每次绘制前先清除上次的绘制，保证地图上只有一个圆
     clearCircleOverlay();
 
     // steps 越大越圆， 圆是由三角形组成的
@@ -42,8 +44,10 @@ export const useQueryNearbyPoints = (mapObj: Ref) => {
 
   const { getPointListByGeoJson, createMarker } = useGeoUtils();
 
+  // marker 图层组
   const markerLayerGroup = ref();
 
+  // 清除 marker 图层组
   const clearMarkerLayerGroup = () => {
     if (markerLayerGroup.value) {
       mapObj.value.removeLayer(markerLayerGroup.value);
@@ -51,7 +55,7 @@ export const useQueryNearbyPoints = (mapObj: Ref) => {
     }
   };
 
-  // 查询附近点位
+  // 查询附近点位并加载到地图上
   const queryNearbyPoints = (point: number[], radius = 2) => {
     clearMarkerLayerGroup();
 
